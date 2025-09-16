28dddbc33ba3a3e2836c43fe4e8c5cf94b6e9e5a
Marthan Lanuzga <tanlanuzga@gmail.com>
2025-09-16 22:35:57 +0800

    updated

--- src/api_client.py
+++ src/api_client.py
@@ -1,33 +1,40 @@
 import requests
 import os
 import json
+import sys
 
 with open("results/filter/output_next.json", "r", encoding="utf-8") as f:
     filtered_text = json.load(f)
 
 
 data = [item["text"] for item in filtered_text]
-
-api_key = os.getenv("OPEN_FDA_KEY")
-
-url = "https://api.fda.gov/drug/label.json"
-params = {
-    "search": data,   # example drug
-    "limit": 1,
-    "api_key": api_key
-}
-
-response = requests.get(url, params=params)
-data = response.json()
-
-if "results" in data:
-    result = data["results"][0]
-    
-
-    print("Purpose:", result.get("purpose", ["N/A"])[0])
-    print("="*100)
-    print("Indications and usage:", result.get("indications_and_usage", ["N/A"])[0])
-    print("="*100)
-    print("Dosage:", result.get("dosage_and_administration", ["N/A"])[0])
+if len(data) == 0:
+    print("No data passed, please try again")
+    sys.exit()
 else:
-    print("No results found")
+    print(data)
+    api_key = os.getenv("OPEN_FDA_KEY")
+
+    url = "https://api.fda.gov/drug/label.json"
+    params = {
+        "search": data,   # example drug
+        "limit": 1,
+        "api_key": api_key
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "results" in data:
+        result = data["results"][0]
+
+
+        print("Purpose:", result.get("purpose", ["N/A"])[0])
+        print("="*100)
+        print("Indications and usage:", result.get("indications_and_usage", ["N/A"])[0])
+        print("="*100)
+        print("Dosage:", result.get("dosage_and_administration", ["N/A"])[0])
+    else:
+        print("No results found")
+
+print("Thank you for using MED_ID!")
