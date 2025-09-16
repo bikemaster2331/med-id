fffac823c803a5d69d887b4383d43254e59b28b1
Marthan Lanuzga <tanlanuzga@gmail.com>
2025-09-16 08:26:38 +0800

    holds api requests

---
+++ src/api_client.py
@@ -0,0 +1,33 @@
+import requests
+import os
+import json
+
+with open("output_next.json", "r", encoding="utf-8") as f:
+    filtered_text = json.load(f)
+
+
+data = [item["text"] for item in filtered_text]
+
+api_key = os.getenv("OPEN_FDA_KEY")
+
+url = "https://api.fda.gov/drug/label.json"
+params = {
+    "search": data,   # example drug
+    "limit": 1,
+    "api_key": api_key
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+
+if "results" in data:
+    result = data["results"][0]
+    
+
+    print("Purpose:", result.get("purpose", ["N/A"])[0])
+    print("="*100)
+    print("Indications and usage:", result.get("indications_and_usage", ["N/A"])[0])
+    print("="*100)
+    print("Dosage:", result.get("dosage_and_administration", ["N/A"])[0])
+else:
+    print("No results found")
